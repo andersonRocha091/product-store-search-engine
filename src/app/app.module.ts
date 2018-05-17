fe0d844dc5d2365ac5d15de6eb1748a03c13c5c4
Anderson Rocha <andersonecomp091@gmail.com>
2018-05-16 21:18:46 -0300

    Configuração de banco de dados no firebase e inclusao do bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularFireModule} from 'angularfire2/index'
 
+import {FirebaseConfig} from './../environments/firebase.config'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(FirebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyB7IwlngoQW4TDnRIlP8Qfw0N6sbhEjUxA",
+    authDomain: "product-store-fbeea.firebaseapp.com",
+    databaseURL: "https://product-store-fbeea.firebaseio.com",
+    projectId: "product-store-fbeea",
+    storageBucket: "product-store-fbeea.appspot.com",
+    messagingSenderId: "35698669504"
+}
